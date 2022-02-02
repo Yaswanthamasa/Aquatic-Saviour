aac532b0b6e76ab2b7069e330ce948a5d3cc15c5
SandeepCHV <cs19b009@iittp.ac.in>
2022-02-02 22:40:59 +0530

    Commit by CS19B005, CS19B009  and CS19B006

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -23,20 +23,22 @@ public class EnemySpawner : MonoBehaviour
     // Update is called once per frame
 
     IEnumerator SpawnEnemies(){
-        yield return new WaitForSeconds(Random.Range(1,3)*2);
-        randomIndex = 1;
-//        Random.Range(0,enemyReference.Length);
-        randomSide = Random.Range(0,2);
-        //spawnedEnemy = Instantiate(enemyReference[randomIndex]);
-        spawnedEnemy = Instantiate(enemyReference[randomIndex]);
 
-        if(randomIndex==0){
-            spawnedEnemy.transform.position = anchorPos.position;
-            spawnedEnemy.GetComponent<Anchor>().speed = -2f;
-        }
-        else{
-            spawnedEnemy.transform.position = minePos.position;
-            spawnedEnemy.GetComponent<Mine>().speed = -2f;
+        while(true){
+            yield return new WaitForSeconds(Random.Range(2,4)*2);
+            randomIndex = Random.Range(0,enemyReference.Length);
+            randomSide = Random.Range(0,2);
+            //spawnedEnemy = Instantiate(enemyReference[randomIndex]);
+            spawnedEnemy = Instantiate(enemyReference[randomIndex]);
+
+            if(randomIndex==0){
+                spawnedEnemy.transform.position = anchorPos.position;
+                spawnedEnemy.GetComponent<Anchor>().speed = -2f;
+            }
+            else{
+                spawnedEnemy.transform.position = minePos.position;
+                spawnedEnemy.GetComponent<Mine>().speed = -2f;
+            }
         }
     }
 }
